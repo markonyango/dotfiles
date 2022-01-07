d06de2457e7d269614853b618fb64864f950bb7f
Mark Onyango <mark.onyango@gmail.com>
2022-01-07 09:27:25 +0100

    Add lsp formatting of current buffer keybinding

--- .config/nvim/lua/bindings.lua
+++ .config/nvim/lua/bindings.lua
@@ -1,6 +1,11 @@
 vim.g.mapleader = " "
 
+-- TELESCOPE
 vim.api.nvim_set_keymap("n", "<leader>ff",":Telescope find_files<cr>", { noremap = false, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>fg",":Telescope live_grep<cr>", { noremap = false, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>fb",":Telescope buffers<cr>", { noremap = false, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>fh",":Telescope help_tags<cr>", { noremap = false, silent = true })
+
+-- LSP BINDINGS
+vim.api.nvim_set_keymap("n", "ff",":lua vim.lsp.buf.formatting()<cr>", { noremap = false, silent = true })
+
