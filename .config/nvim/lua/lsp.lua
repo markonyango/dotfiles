925763aee20b030da3e1955a44f458cc3f24f547
Mark Onyango <mark.onyango@gmail.com>
2022-01-17 11:21:08 +0100

    Execute on_attach callback when Rust or Typescript/JavaScript files are opened

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -38,13 +38,13 @@ lsp_installer.settings({
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
--- local servers = { 'rust_analyzer', 'tsserver' }
--- for _, lsp in ipairs(servers) do
---   nvim_lsp[lsp].setup {
---     on_attach = on_attach,
---     flags = {
---       debounce_text_changes = 150,
---     }
---   }
--- end
+local servers = { 'rust_analyzer', 'tsserver' }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
+end
 --
